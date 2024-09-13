69ce038b443a86fe963176ce7b673c48648f53e0
SoyManuelPerez <Soy_ManuelPerez@outlook.com>
2024-09-13 18:14:58 -0500

    Cambios en Producto

--- models/Producto.js
+++ models/Producto.js
@@ -2,7 +2,15 @@ const mongoose = require('mongoose')
 const Producto = new mongoose.Schema({
     Producto : String,
     Imagen : String,
-    Cantidad : Number,
+    T30 : Number,
+    T32 : Number,
+    T34 : Number,
+    T36 : Number,
+    T38 : Number,
+    T40 : Number,
+    T42 : Number,
+    T44 : Number,
+    T46 : Number,
     Vendidos : Number,
     Ingreso : Number,
     Precio : Number
