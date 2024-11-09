08e46aaf30e2cbeadf9a550aad4ad11b01c0335c
SoyManuelPerez <Soy_ManuelPerez@outlook.com>
2024-11-09 11:40:45 -0500

    Mensaje de pdf generado o error

--- public/js/pdf.js
+++ public/js/pdf.js
@@ -1,15 +1,32 @@
 function descargarPDF(tablaId) {
-    // Solicita los datos al servidor
-    fetch(`/descargarPDF?tabla=${tablaId}`)
-      .then(response => response.blob())
-      .then(blob => {
-        // Crea un enlace para descargar el archivo
-        const url = window.URL.createObjectURL(blob);
-        const a = document.createElement('a');
-        a.href = url;
-        a.download = `${tablaId}.pdf`;
-        a.click();
-      })
-      .catch(error => console.error("Error al descargar el PDF:", error));
-  }
-  
+  // Muestra el mensaje de carga y oculta cualquier mensaje de error previo
+  document.getElementById('mensajeCarga').style.display = 'block';
+  document.getElementById('mensajeError').style.display = 'none';
+
+  // Solicita el PDF al servidor
+  fetch(`/descargarPDF?tabla=${tablaId}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("Error en la respuesta del servidor");
+      }
+      return response.blob();
+    })
+    .then(blob => {
+      // Crea un enlace para descargar el archivo
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = `${tablaId}.pdf`;
+      a.click();
+
+      // Oculta el mensaje de carga después de la descarga
+      document.getElementById('mensajeCarga').style.display = 'none';
+    })
+    .catch(error => {
+      console.error("Error al descargar el PDF:", error);
+
+      // Muestra mensaje de error y oculta el mensaje de carga
+      document.getElementById('mensajeCarga').style.display = 'none';
+      document.getElementById('mensajeError').style.display = 'block';
+    });
+}
