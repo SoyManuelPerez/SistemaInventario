08c050d42b2020a5ce1e1199b3493522f3c70415
Manuel Perez <Soy_ManuelPerez@outlook.com>
2024-11-24 13:32:39 -0500

    pdf ventas

--- controllers/ControllerVentas.js
+++ controllers/ControllerVentas.js
@@ -31,3 +31,23 @@ module.exports.mostrar = (req, res) => {
     res.status(500).send('Error mostrando datos');
   });
 }
+// Controlador para servir el PDF
+exports.pdf = (req, res) => {
+  // Asegúrate de que `req.query.file` contenga el nombre del archivo
+  const fileName = req.query.file;
+
+  if (!fileName) {
+    return res.status(400).send('El archivo no se especificó.');
+  }
+
+  // Ruta absoluta al archivo
+  const filePath = path.join(__dirname, '../public/pdf', fileName);
+
+  // Enviar el archivo al cliente
+  res.sendFile(filePath, (err) => {
+    if (err) {
+      console.error('Error al enviar el archivo:', err);
+      res.status(404).send('Archivo no encontrado.');
+    }
+  });
+};
--- routers/Main.routes.js
+++ routers/Main.routes.js
@@ -25,6 +25,8 @@ router.get('/Ventas',Ventas.mostrar,(req,res)=>{
 router.get('/Estadisticas',Estadistica.mostrar,(req,res)=>{
     res.render('Estadistica')
 });
+router.get('/pdf',Ventas.pdf,(req,res)=>{
+});
 router.get('/Pedido',Pedido.mostrar,(req,res)=>{
     res.render('Pedido')
 });
