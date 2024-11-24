5bee666e1a622306e1358620642c998fcea660d2
Manuel Perez <Soy_ManuelPerez@outlook.com>
2024-11-24 07:14:06 -0500

    Pdf en ventas

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.set('view engine', 'ejs',)
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname,'public')))
+app.use('/pdf', express.static(path.join(__dirname, 'public/pdf')));
 //Middlewares
 app.use(cookieParser());
 //Rutas
