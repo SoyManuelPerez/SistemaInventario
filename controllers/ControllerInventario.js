ee33bd8842307dd7b04768f41b5d168be001bbe7
Manuel Alejandro Perez Benitez <Soy_ManuelPerez@outlook.com>
2025-10-02 10:45:29 -0500

    Clientes

--- controllers/ControllerInventario.js
+++ controllers/ControllerInventario.js
@@ -496,25 +496,11 @@ function configureGitRemote(callback) {
 }
 //Mostrar Catalogo
 module.exports.mostrarCliente = (req, res) => {
-    const token = req.cookies.jwt;
-  let User = "";
-  if (token) {
-    jsonwebtoken.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-      if (err) {
-        return res.redirect("/");
-      }
-      User = decoded.user;
-    }); 
-  }
   Promise.all([
     Productos.find({ Cantidad: { $gt: 0 } }),
-    Usuario.find({ user: User })
-  ]).then(([Producto, Usuario]) => {
-    const tipoUsuario = Usuario.length > 0 ? Usuario[0].type : null;
-    res.render('Catalogo', {
+  ]).then(([Producto]) => {
+    res.render('Cliente', {
         Producto: Producto,
-        tipoUsuario: tipoUsuario,
-        User:User
     });
   })
   .catch(err => {
